a7a70fa98a766a0db21699273a80715e5f880b14
onodera <cxiaoyesi745@gmail.com>
2021-09-12 23:41:33 +0900

    initial Commit

--- MyCamera/ViewController.swift
+++ MyCamera/ViewController.swift
@@ -7,13 +7,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePickerControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBOutlet weak var pictureImage: UIImageView!
+    
+    @IBAction func cameraButtonAction(_ sender: Any) {
+        //カメラかフォトライブラリーどちらから画像を取得するかを選択
+        let alertController = UIAlertController(title: "確認", message: "選択してください", preferredStyle: .actionSheet)
+        
+        //カメラを起動するための選択肢を定義
+        let cameraAction = UIAlertAction(title: "カメラ", style: .default, handler: nil)
+        alertController.addAction(cameraAction)
+        //フォトライブラリーを起動するための選択肢を定義
+        let photoLibraryAction = UIAlertAction(title: "フォトライブラリー", style: .default, handler: nil)
+        alertController.addAction(photoLibraryAction)
+        
+        //キャンセルの選択肢を定義
+        let cancelAction = UIAlertAction(title: "キャンセル", style: .default, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        //iPadで落ちてしまう対策
+        alertController.popoverPresentationController?.sourceView = view
+        
+        //選択肢を画面に表示
+        present(alertController, animated: true, completion: nil)
+        
+    }
+    @IBAction func shareButtonAction(_ sender: Any) {
+        
+        if let shareImage = pictureImage.image {
+            
+            //UIActivityViewControllerにシェア画像を渡す
+            let shareItems = [shareImage]
+            //UIActivityViewControllerにシェア画像を渡す
+            let controller = UIActivityViewController(activityItems: shareItems, applicationActivities: nil)
+            //iPadで落ちてしまう対策
+            controller.popoverPresentationController?.sourceView = view
+            //UIActivityViewControllerを表示
+            present(controller, animated: true, completion: nil)
+        }
+        
+        }
+    
+    //(1)撮影が終わった時に呼ばれるdelegateメソッド
+    func  imagePickerController(_ picker:UIImagePickerController , didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        //(2)撮影した画像を配置したpictureImageに渡す
+        pictureImage.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        //(3)モーダルビューを閉じる
+        dismiss(animated: true, completion: nil)
+    
+    }
 }
 
